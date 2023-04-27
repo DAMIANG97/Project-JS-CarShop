c26fe2fb58f4608c12181161eea7e4c3e377ed6f
DAMIANG97 <damiangrabowski975@gmail.com>
2023-04-27 18:14:36 +0200

    Summary form
    - Add value for radio buttons.
    - Bugfix (reset accesories value on click back button)
    - Change some variables to object (index.js)
    - Editing script (index.js)to moving data from the list to the summary.
    - Added components to purchase summary (summary.html).
    - Add script summary.js to show date for summary.
    - Summary page styling (summary.css).

--- index.js
+++ index.js
@@ -10,12 +10,13 @@ const $errorMsg = document.getElementById("error-msg");
 const leasingRadio = document.getElementById("leasing-radio");
 const cashRadio = document.getElementById("cash-radio");
 
-const $wheelMinus = document.getElementById("wheelMinus");
-const $wheelPlus = document.getElementById("wheelPlus");
-const $guaranteeMinus = document.getElementById("guaranteeMinus");
-const $guaranteePlus = document.getElementById("guaranteePlus");
-const $wheelsAmount = document.getElementById("wheelsAmount");
-const $guaranteeYears = document.getElementById("guaranteeYears");
+const $back = document.getElementById("back");
+var $wheelMinus = document.getElementById("wheelMinus");
+var $wheelPlus = document.getElementById("wheelPlus");
+var $guaranteeMinus = document.getElementById("guaranteeMinus");
+var $guaranteePlus = document.getElementById("guaranteePlus");
+var $wheelsAmount = document.getElementById("wheelsAmount");
+var $guaranteeYears = document.getElementById("guaranteeYears");
 
 ///////////////////////////////////// go to purchase form
 $offer.forEach((e2) => {
@@ -29,51 +30,68 @@ $offer.forEach((e2) => {
   });
 });
 //////////////////////////////////// go to purchase form
-const mustang = "Ford Mustang";
-const audiTT = "Audi TT";
-const audiA6 = "Audi A6";
-const maybach = "Mercedes Maybach";
-var priceMustang = 50000;
-var priceTT = 15000;
-var priceA6 = 50000;
-var priceMaybach = 350000;
+let mustang = {
+  price: 50000,
+  carName: "Ford Mustang",
+  carImage: "assets/fordMustang.jpg",
+};
+let audiTT = {
+  price: 15000,
+  carName: "Audi TT",
+  carImage: "assets/audiTT.png",
+};
+let audiA6 = {
+  price: 50000,
+  carName: "Audi A6",
+  carImage: "assets/audiA6.png",
+};
+let maybach = {
+  price: 350000,
+  carName: "Mercedes Maybach",
+  carImage: "assets/mercedesMAYBACH.png",
+};
+
 var price;
 var wheelsAmount = 0;
 var guaranteeYears = 0;
-
+var carName;
+var carImage;
 ///////////////////////////////////////////////////////////////////// follow price and name
 
 $fordMustang.addEventListener("click", () => {
-  price = priceMustang;
-  const $price = (document.getElementById(
-    "totalprice"
-  ).innerHTML = `Cena całkowita: ${price}  PLN`);
-  const $carName = (document.getElementById("carChoose").innerHTML =
-    "Wybrane auto: " + mustang);
+  price = mustang.price;
+  carName = mustang.carName;
+  carImage = mustang.carImage;
+  $price = document.getElementById("totalprice").innerHTML = `Cena całkowita: ${price}  PLN`;
+  $carName = document.getElementById("carChoose").innerHTML = "Wybrane auto: " + carName;
 });
 $audiTT.addEventListener("click", () => {
-  price = priceTT;
-  const $price = (document.getElementById(
-    "totalprice"
-  ).innerHTML = `Cena całkowita: ${price} PLN`);
-  const $carName = (document.getElementById("carChoose").innerHTML =
-    "Wybrane auto: " + audiTT);
+  price = audiTT.price;
+  carName = audiTT.carName;
+  carImage = audiTT.carImage;
+  $price = document.getElementById("totalprice").innerHTML = `Cena całkowita: ${price} PLN`;
+  $carName = document.getElementById("carChoose").innerHTML = "Wybrane auto: " + carName;
 });
 $audiA6.addEventListener("click", () => {
-  price = priceA6;
-  const $price = (document.getElementById(
-    "totalprice"
-  ).innerHTML = `Cena całkowita: ${price} PLN`);
-  const $carName = (document.getElementById("carChoose").innerHTML =
-    "Wybrane auto: " + audiA6);
+  price = audiA6.price;
+  carName = audiA6.carName;
+  carImage = audiA6.carImage;
+  $price = document.getElementById("totalprice").innerHTML = `Cena całkowita: ${price} PLN`;
+  $carName = document.getElementById("carChoose").innerHTML = "Wybrane auto: " + carName;
 });
 $mercedesMaybach.addEventListener("click", () => {
-  price = priceMaybach;
-  const $price = (document.getElementById(
-    "totalprice"
-  ).innerHTML = `Cena całkowita: ${price} PLN`);
-  const $carName = (document.getElementById("carChoose").innerHTML =
-    "Wybrane auto: " + maybach);
+  price = maybach.price;
+  carName = maybach.carName;
+  carImage = maybach.carImage;
+  c$price = document.getElementById("totalprice").innerHTML = `Cena całkowita: ${price} PLN`;
+  $carName = document.getElementById("carChoose").innerHTML = "Wybrane auto: " + carName;
+});
+
+$back.addEventListener("click", () => {
+  wheelsAmount = 0;
+  guaranteeYears = 0;
+  $wheelsAmount = document.getElementById("wheelsAmount").innerHTML = `Opony zimowe: ${wheelsAmount} szt`;
+  $guaranteeYears = document.getElementById("guaranteeYears").innerHTML = `Dodatkowy rok gwarancji: ${guaranteeYears} lat`;
 });
 
 ////////////////////////////////////////////////////////////////// follow price and name
@@ -82,50 +100,34 @@ $mercedesMaybach.addEventListener("click", () => {
 
 $wheelMinus.addEventListener("click", () => {
   price = price - 2000;
-  const $price = (document.getElementById(
-    "totalprice"
-  ).innerHTML = `Cena całkowita: ${price} PLN`);
+  $price = document.getElementById("totalprice").innerHTML = `Cena całkowita: ${price} PLN`;
 
   wheelsAmount = wheelsAmount - 1;
-  const $wheelsAmount = (document.getElementById(
-    "wheelsAmount"
-  ).innerHTML = `Opony zimowe: ${wheelsAmount} szt`);
+  $wheelsAmount = document.getElementById("wheelsAmount").innerHTML = `Opony zimowe: ${wheelsAmount} szt`;
 });
 
 $wheelPlus.addEventListener("click", () => {
   price = price + 2000;
-  const $price = (document.getElementById(
-    "totalprice"
-  ).innerHTML = `Cena całkowita: ${price} PLN`);
+  $price = document.getElementById("totalprice").innerHTML = `Cena całkowita: ${price} PLN`;
 
   wheelsAmount = wheelsAmount + 1;
-  const $wheelsAmount = (document.getElementById(
-    "wheelsAmount"
-  ).innerHTML = `Opony zimowe: ${wheelsAmount} szt`);
+  $wheelsAmount = document.getElementById("wheelsAmount").innerHTML = `Opony zimowe: ${wheelsAmount} szt`;
 });
 
 $guaranteeMinus.addEventListener("click", () => {
   price = price - 5000;
-  const $price = (document.getElementById(
-    "totalprice"
-  ).innerHTML = `Cena całkowita: ${price} PLN`);
+  $price = document.getElementById("totalprice").innerHTML = `Cena całkowita: ${price} PLN`;
 
   guaranteeYears = guaranteeYears - 1;
-  const $guaranteeYears = (document.getElementById(
-    "guaranteeYears"
-  ).innerHTML = `Dodatkowy rok gwarancji ${guaranteeYears} lat`);
+  $guaranteeYears = document.getElementById("guaranteeYears").innerHTML = `Dodatkowy rok gwarancji ${guaranteeYears} lat`;
 });
 
 $guaranteePlus.addEventListener("click", () => {
   price = price + 5000;
-  const $price = (document.getElementById(
-    "totalprice"
-  ).innerHTML = `Cena całkowita: ${price} PLN`);
+  $price = document.getElementById("totalprice").innerHTML = `Cena całkowita: ${price} PLN`;
 
   guaranteeYears = guaranteeYears + 1;
-  const $guaranteeYears = (document.getElementById(
-    "guaranteeYears"
-  ).innerHTML = `Dodatkowy rok gwarancji: ${guaranteeYears} lat`);
+  $guaranteeYears = document.getElementById("guaranteeYears").innerHTML = `Dodatkowy rok gwarancji: ${guaranteeYears} lat`;
 });
 
 /////////////////////////////////////////////////////////////////// accesories
@@ -137,20 +139,26 @@ $buy.addEventListener("click", () => {
   const placeInput = document.getElementById("place").value.trim();
   const dateInput = document.getElementById("date").value;
 
-  if (
-    nameInput.length === 0 ||
-    placeInput.length === 0 ||
-    dateInput.length === 0
-  ) {
+  if (nameInput.length === 0 || placeInput.length === 0 || dateInput.length === 0) {
     $errorMsg.textContent = "Error: All fields are required!";
   } else if (nameInput.split(" ").length !== 2) {
-    $errorMsg.textContent =
-      "Error: Name field must contain two strings separated by a space!";
+    $errorMsg.textContent = "Error: Name field must contain two strings separated by a space!";
   } else if (!leasingRadio.checked && !cashRadio.checked) {
     $errorMsg.textContent = "Error: Please select a payment method!";
   } else {
+    sessionStorage.setItem("Price", price);
+    sessionStorage.setItem("CarName", carName);
+    sessionStorage.setItem("CarImage", carImage);
     window.location.href = "summary.html";
     $errorMsg.textContent = "";
+    const $radioButtons = document.querySelectorAll('input[name="payChoose"]');
+    for (const radioButton of $radioButtons) {
+      if (radioButton.checked) {
+        selectedPay = radioButton.value;
+        sessionStorage.setItem("SelectPay", selectedPay);
+      }
+      break;
+    }
   }
 });
 
